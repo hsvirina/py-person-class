0fe9a75038ba506ce7147de90b31241b76b2980d
Hanna Svirina <26anna07@ukr.net>
2025-06-01 18:16:33 +0200

    fix solution

--- app/main.py
+++ app/main.py
@@ -8,14 +8,22 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
+    Person.people.clear()
+
     for person_dict in people:
         Person(person_dict["name"], person_dict["age"])
 
     for person_dict in people:
         person_obj = Person.people[person_dict["name"]]
+
         if "wife" in person_dict and person_dict["wife"]:
-            person_obj.wife = Person.people[person_dict["wife"]]
+            wife_name = person_dict["wife"]
+            if wife_name in Person.people:
+                person_obj.wife = Person.people[wife_name]
+
         elif "husband" in person_dict and person_dict["husband"]:
-            person_obj.husband = Person.people[person_dict["husband"]]
+            husband_name = person_dict["husband"]
+            if husband_name in Person.people:
+                person_obj.husband = Person.people[husband_name]
 
     return [Person.people[p["name"]] for p in people]
